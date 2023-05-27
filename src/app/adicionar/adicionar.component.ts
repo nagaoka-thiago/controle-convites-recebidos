8ebcb4b1119c0e254e98a6b6804062bd0fa00a94
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2023-05-27 16:07:35 -0300

    adicionei o componente cadastrar-alterar com o uso da diretiva ngModel e do materialize. Também adicionei o componente adicionar, que é um botão em que é usado o materialize. Foi mudado o convite, para que o convite seja alimentado através de data binding.

---
+++ src/app/adicionar/adicionar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar',
+  templateUrl: './adicionar.component.html',
+  styleUrls: ['./adicionar.component.css']
+})
+export class AdicionarComponent {
+  clicado(): void {
+    console.log("Componente adicionar foi clicado!");
+  }
+}
